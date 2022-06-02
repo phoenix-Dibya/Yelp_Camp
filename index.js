c3afea1b8565250bd460bba6d21a769bf52d00c1
Phoenix@Dibya <dibyaranjansahu.mail@gmail.com>
2022-06-02 11:23:07 +0530

    added start scripts

--- index.js
+++ index.js
@@ -104,6 +104,8 @@ app.use((err, req, res, next) => {
     res.status(status).render("error", { err });
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log("Welcome to new project");
 });
